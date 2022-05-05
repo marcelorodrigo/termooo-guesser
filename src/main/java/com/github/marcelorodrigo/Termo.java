2d5e7719e27519366337158ae93f7cde9d8eec71
Marcelo Wiebbelling <mrodrigow@gmail.com>
2022-05-05 20:29:32 +0200

    Removing unused imports

--- src/main/java/com/github/marcelorodrigo/Termo.java
+++ src/main/java/com/github/marcelorodrigo/Termo.java
@@ -6,7 +6,6 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
